b15578587cfd0fa70a80df87b163c293a939d74e
Víctor <victorescobargonza@gmail.com>
2015-06-14 17:03:26 +0200

    Adding Contact & Invoice classes

---
+++ src/Vistiyos/ERP/ERPBundle/Entity/Contact.php
@@ -0,0 +1,314 @@
+<?php
+
+namespace Vistiyos\ERP\ERPBundle\Entity;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Contact
+ *
+ * @ORM\Table()
+ * @ORM\Entity
+ */
+class Contact
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="business_name", type="string", length=255)
+     */
+    private $businessName;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="tax_id", type="string", length=255)
+     */
+    private $taxId;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="email", type="string", length=255)
+     */
+    private $email;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="address1", type="string", length=255)
+     */
+    private $addressº;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="address2", type="string", length=255)
+     */
+    private $address2;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="city", type="string", length=255)
+     */
+    private $city;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="region", type="string", length=255)
+     */
+    private $region;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="postcode", type="string", length=255)
+     */
+    private $postcode;
+
+    /**
+     * @var ArrayCollection
+     */
+    private $invoices;
+
+    public function __construct()
+    {
+        $this->invoices = new ArrayCollection();
+    }
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return Contact
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set businessName
+     *
+     * @param string $businessName
+     * @return Contact
+     */
+    public function setBusinessName($businessName)
+    {
+        $this->businessName = $businessName;
+
+        return $this;
+    }
+
+    /**
+     * Get businessName
+     *
+     * @return string
+     */
+    public function getBusinessName()
+    {
+        return $this->businessName;
+    }
+
+    /**
+     * Set taxId
+     *
+     * @param string $taxId
+     * @return Contact
+     */
+    public function setTaxId($taxId)
+    {
+        $this->taxId = $taxId;
+
+        return $this;
+    }
+
+    /**
+     * Get taxId
+     *
+     * @return string
+     */
+    public function getTaxId()
+    {
+        return $this->taxId;
+    }
+
+    /**
+     * Set email
+     *
+     * @param string $email
+     * @return Contact
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * Get email
+     *
+     * @return string
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * Set addressº
+     *
+     * @param string $addressº
+     * @return Contact
+     */
+    public function setAddressº($addressº)
+    {
+        $this->addressº = $addressº;
+
+        return $this;
+    }
+
+    /**
+     * Get addressº
+     *
+     * @return string
+     */
+    public function getAddressº()
+    {
+        return $this->addressº;
+    }
+
+    /**
+     * Set address2
+     *
+     * @param string $address2
+     * @return Contact
+     */
+    public function setAddress2($address2)
+    {
+        $this->address2 = $address2;
+
+        return $this;
+    }
+
+    /**
+     * Get address2
+     *
+     * @return string
+     */
+    public function getAddress2()
+    {
+        return $this->address2;
+    }
+
+    /**
+     * Set city
+     *
+     * @param string $city
+     * @return Contact
+     */
+    public function setCity($city)
+    {
+        $this->city = $city;
+
+        return $this;
+    }
+
+    /**
+     * Get city
+     *
+     * @return string
+     */
+    public function getCity()
+    {
+        return $this->city;
+    }
+
+    /**
+     * Set region
+     *
+     * @param string $region
+     * @return Contact
+     */
+    public function setRegion($region)
+    {
+        $this->region = $region;
+
+        return $this;
+    }
+
+    /**
+     * Get region
+     *
+     * @return string
+     */
+    public function getRegion()
+    {
+        return $this->region;
+    }
+
+    /**
+     * Set postcode
+     *
+     * @param string $postcode
+     * @return Contact
+     */
+    public function setPostcode($postcode)
+    {
+        $this->postcode = $postcode;
+
+        return $this;
+    }
+
+    /**
+     * Get postcode
+     *
+     * @return string
+     */
+    public function getPostcode()
+    {
+        return $this->postcode;
+    }
+}
---
+++ src/Vistiyos/ERP/ERPBundle/Entity/Invoice.php
@@ -0,0 +1,244 @@
+<?php
+
+namespace Vistiyos\ERP\ERPBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Invoice
+ *
+ * @ORM\Table()
+ * @ORM\Entity
+ */
+class Invoice
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="totalNet", type="decimal")
+     */
+    private $totalNet;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="totalVat", type="decimal")
+     */
+    private $totalVat;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="total", type="decimal")
+     */
+    private $total;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="timeAdded", type="datetimetz")
+     */
+    private $timeAdded;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="timeModified", type="datetimetz")
+     */
+    private $timeModified;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="status", type="smallint")
+     */
+    private $status;
+
+    /**
+     * @var Contact
+     * @ORM\ManyToOne(targetEntity="Vistiyos\ERP\ERPBundle\Entity\Contact", inversedBy="invoices")
+     * @ORM\JoinColumn(name="contact_id",referencedColumnName="id")
+     */
+    private $contact;
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set totalNet
+     *
+     * @param string $totalNet
+     * @return Invoice
+     */
+    public function setTotalNet($totalNet)
+    {
+        $this->totalNet = $totalNet;
+
+        return $this;
+    }
+
+    /**
+     * Get totalNet
+     *
+     * @return string
+     */
+    public function getTotalNet()
+    {
+        return $this->totalNet;
+    }
+
+    /**
+     * Set totalVat
+     *
+     * @param string $totalVat
+     * @return Invoice
+     */
+    public function setTotalVat($totalVat)
+    {
+        $this->totalVat = $totalVat;
+
+        return $this;
+    }
+
+    /**
+     * Get totalVat
+     *
+     * @return string
+     */
+    public function getTotalVat()
+    {
+        return $this->totalVat;
+    }
+
+    /**
+     * Set total
+     *
+     * @param string $total
+     * @return Invoice
+     */
+    public function setTotal($total)
+    {
+        $this->total = $total;
+
+        return $this;
+    }
+
+    /**
+     * Get total
+     *
+     * @return string
+     */
+    public function getTotal()
+    {
+        return $this->total;
+    }
+
+    /**
+     * Set timeAdded
+     *
+     * @param \DateTime $timeAdded
+     * @return Invoice
+     */
+    public function setTimeAdded($timeAdded)
+    {
+        $this->timeAdded = $timeAdded;
+
+        return $this;
+    }
+
+    /**
+     * Get timeAdded
+     *
+     * @return \DateTime
+     */
+    public function getTimeAdded()
+    {
+        return $this->timeAdded;
+    }
+
+    /**
+     * Set timeModified
+     *
+     * @param \DateTime $timeModified
+     * @return Invoice
+     */
+    public function setTimeModified($timeModified)
+    {
+        $this->timeModified = $timeModified;
+
+        return $this;
+    }
+
+    /**
+     * Get timeModified
+     *
+     * @return \DateTime
+     */
+    public function getTimeModified()
+    {
+        return $this->timeModified;
+    }
+
+    /**
+     * Set status
+     *
+     * @param integer $status
+     * @return Invoice
+     */
+    public function setStatus($status)
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+
+    /**
+     * Get status
+     *
+     * @return integer
+     */
+    public function getStatus()
+    {
+        return $this->status;
+    }
+
+    /**
+     * Get contact
+     *
+     * @return Contact
+     */
+    public function getContact()
+    {
+        return $this->contact;
+    }
+
+    /**
+     * Set contact
+     *
+     * @param Contact $contact Contact
+     * 
+     * @return $this
+     */
+    public function setContact(Contact $contact)
+    {
+        $this->contact = $contact;
+
+        return $this;
+    }
+}
